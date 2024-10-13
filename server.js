d5b7e952c18272e130ba46f0677484d579edc1c5
Md Maruf <maruf509599@gmail.com>
2024-10-13 16:44:49 +0600

    update

--- server.js
+++ server.js
@@ -7,10 +7,12 @@ import cookieParser from "cookie-parser";
 import { notFound, errorHandler } from "./middleware/errorMiddleware.js";
 import userRoutes from "./routes/userRoutes.js";
 import homeRoutes from "./routes/homeRoutes.js";
-import contactRoutes from "./routes/contactRoutes.js";
+// import contactRoutes from "./routes/contactRoutes.js";
 import bannerRoutes from "./routes/bannerRoutes.js";
 import aboutRoutes from "./routes/aboutRoutes.js";
 import productRoutes from "./routes/productRoutes.js";
+import formRoutes from "./routes/formRoutes.js";
+import contactPageRoutes from "./routes/contactPageRoutes.js";
 import cors from "cors";
 
 const port = process.env.PORT || 5000;
@@ -37,10 +39,12 @@ app.use(cookieParser());
 // Routes
 app.use("/api/users", userRoutes);
 app.use("/api/home", homeRoutes);
-app.use("/api/contact", contactRoutes);
+// app.use("/api/contact", contactRoutes);
 app.use("/api/banner", bannerRoutes);
 app.use("/api/about", aboutRoutes);
 app.use("/api/products", productRoutes);
+app.use("/api/contact", contactPageRoutes);
+app.use("/api/form", formRoutes);
 
 // Development fallback for serving an initial response
 app.get("/", (req, res) => {
